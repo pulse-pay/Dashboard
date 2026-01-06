cdf422092042624ece54ded1b06ecd24558fc40e
Ash310u <ashutoshsaha233@gmail.com>
2026-01-06 19:12:02 +0530

    boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className='h-screen'>
+        <h1 className='text-3xl font-bold text-center'>Hello World</h1>
+      </div>
+    </>
+  )
+};
+
+export default App;
